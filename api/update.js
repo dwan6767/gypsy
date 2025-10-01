fc174e19c0ce464c522e4c6fb910f7ed5385937c
dwan6767 <dwanshikari@gmail.com>
2025-10-01 19:15:43 +0530

    changes

--- api/update.js
+++ api/update.js
@@ -1,5 +1,7 @@
 // api/update.js
 
+// api/update.js
+
 // Store the latest HTML sent by ESP
 let latestHTML = `
 <!DOCTYPE html>
@@ -23,10 +25,8 @@ let latestHTML = `
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      // Parse JSON body
+      // Parse JSON body from ESP8266
       const data = req.body;
-      // If Vercel parses JSON automatically, req.body is object
-      // Otherwise, use: const data = JSON.parse(req.body);
 
       if (data.html) {
         latestHTML = data.html;
@@ -38,10 +38,10 @@ export default async function handler(req, res) {
       res.status(400).json({ message: "Invalid JSON" });
     }
   } else if (req.method === "GET") {
-    // Serve the latest HTML
-    res.setHeader("Content-Type", "text/html");
-    res.status(200).send(latestHTML);
+    // Return the latest HTML as JSON for frontend
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json({ html: latestHTML });
   } else {
-    res.status(405).send("Method Not Allowed"); 
+    res.status(405).json({ message: "Method Not Allowed" });
   }
 }
