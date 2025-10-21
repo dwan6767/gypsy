9da67f7f719551ac8fb0aa3d7f0a5ff8ad28e7a7
dwan6767 <dwanshikari@.com>
2025-10-21 10:42:02 +0530

    changes

--- api/update.js
+++ api/update.js
@@ -1,11 +1,11 @@
+// pages/index.js or api/index.js
 import fs from 'fs';
 import path from 'path';
 
 const file = path.resolve('/tmp/url.json');
 
 export default function handler(req, res) {
-  let url = null;
-
+  let url;
   try {
     if (fs.existsSync(file)) {
       const data = JSON.parse(fs.readFileSync(file, 'utf8'));
@@ -17,8 +17,8 @@ export default function handler(req, res) {
 
   if (!url) return res.status(404).send('No active tunnel');
 
-  // Redirect to ngrok URL
   res.writeHead(302, { Location: url });
   res.end();
 }
+
 
